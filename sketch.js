ab6c98dc30e1b7fea19eae5d5bb22fe3f96a58fe
Supercoderaj <73371222+Supercoderaj@users.noreply.github.com>
2020-11-27 20:03:14 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ var speed, weight;
 function setup() {
   createCanvas(1600,400);
   car = createSprite(50, 200, 50, 50);
-  wall = createSprite(1500, 200, 60, hight/2 );
+  wall = createSprite(1500, 200, 60, height/2 );
 
   speed = random(55,90)
   weight = random(400,1500)
@@ -16,6 +16,28 @@ function setup() {
 
 function draw() {
   background(255,255,255); 
-  bounceOff(car,wall) 
+  if (istouching(car,wall)){
+
+    car.velocityX = 0
+  
+  var deformation = (0.5*weight*speed*speed)/22500
+  console.log(deformation);
+  if (deformation > 180) {
+    car.shapeColor = "red"
+
+  }
+else
+if (deformation<180 && deformation> 100){
+
+  car.shapeColor= "yellow"
+
+}
+else
+if (deformation<100){
+
+   car.shapeColor = "green"
+
+}
+  }
   drawSprites();
 }
